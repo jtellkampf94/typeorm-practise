eafb909ed85265be0896f59bc47e2d7af95331e3
Jonathan <jtellkampf@gmail.com>
2021-06-29 20:13:28 +0100

    seperated schema generation to its own module

--- graphql-ts-server-boilerplate/src/index.ts
+++ graphql-ts-server-boilerplate/src/index.ts
@@ -1,28 +1,14 @@
 import "reflect-metadata";
-import { GraphQLSchema } from "graphql";
-import { createConnection } from "typeorm";
-import { importSchema } from "graphql-import";
 import { GraphQLServer } from "graphql-yoga";
-import * as path from "path";
-import * as fs from "fs";
-import { mergeSchemas, makeExecutableSchema } from "graphql-tools";
+import { createConnection } from "typeorm";
 
 import { confirmEmail } from "./routes/confirmEmail";
 import { redis } from "./redis";
+import { generateSchema } from "./utils/generateSchema";
 
 export const startServer = async () => {
-  const schemas: GraphQLSchema[] = [];
-  const folders = fs.readdirSync(path.join(__dirname, "./modules"));
-  folders.forEach(folder => {
-    const { resolvers } = require(`./modules/${folder}/resolvers`);
-    const typeDefs = importSchema(
-      path.join(__dirname, `./modules/${folder}/schema.graphql`)
-    );
-    schemas.push(makeExecutableSchema({ resolvers, typeDefs }));
-  });
-
   const server = new GraphQLServer({
-    schema: mergeSchemas({ schemas }),
+    schema: generateSchema(),
     context: ({ request }) => ({
       redis,
       url: request.protocol + "://" + request.get("host")
