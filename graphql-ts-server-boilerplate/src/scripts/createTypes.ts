b724421e85f437e98b0ce0487b0cb3cc193692c7
Jonathan <jtellkampf@gmail.com>
2021-06-29 20:26:18 +0100

    Created createTypes file to generate schema types

---
+++ graphql-ts-server-boilerplate/src/scripts/createTypes.ts
@@ -0,0 +1,15 @@
+import * as fs from "fs";
+import * as path from "path";
+
+import { generateSchema } from "./../utils/generateSchema";
+import { generateNamespace } from "@gql2ts/from-schema";
+
+const typescriptTypes = generateNamespace("GQL", generateSchema());
+
+fs.writeFile(
+  path.join(__dirname, "../types/schema.d.ts"),
+  typescriptTypes,
+  err => {
+    console.log(err);
+  }
+);
